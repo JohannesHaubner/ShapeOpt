e722c3837119cc7b3c83ba20d22308196cbd35d7
JohannesHaubner <haubnerj@simula.no>
2022-12-19 15:54:23 +0100

    add run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -p dgx2q
+#SBATCH -N 1
+#SBATCH -n 8
+#SBATCH -o slurm.%N.%j.out
+#SBATCH -e slurm.%N.%j.err
+
+module purge
+module load singularity-ce
+
+mpiexec -n 4 singularity exec ghcr.io/johanneshaubner/shapeopt:latest python3 /home/haubnerj/ShapeOpt/example/FSI/main.py
