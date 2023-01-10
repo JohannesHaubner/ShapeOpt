5c670f3f06cebab86dcaa659bd4db80ae37d5a52
JohannesHaubner <haubnerj@simula.no>
2023-01-10 14:51:36 +0100

    update run.sh-file, singularity working

--- ex3/run.sh
+++ ex3/run.sh
@@ -10,6 +10,8 @@ module load singularity-ce
 module load mpich-3.3.2
 
 export SINGULARITYENV_CXX=/usr/bin/c++
+export SINGULARITY_DOCKER_USERNAME=johanneshaubner
+export SINGULARITY_DOCKER_PASSWORD=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
 
 # in SINGULARITY_DOCKER_PASSWORD there is a github-token that has just access to read:packages
-srun -n 4 SINGULARITY_DOCKER_USERNAME=johanneshaubner SINGULARITY_DOCKER_PASSWORD=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 singularity exec docker://ghcr.io/johanneshaubner/shapeopt:latest python3 /home/haubnerj/shapeopt/example/FSI/main.py
+srun -n 4 singularity exec docker://ghcr.io/johanneshaubner/shapeopt:latest python3 /home/haubnerj/shapeopt/example/FSI/main.py
