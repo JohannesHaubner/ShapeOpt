0d3e7e7c793d9437c658659bbe58a85170273805
JohannesHaubner <haubnerj@simula.no>
2023-01-12 08:31:56 +0000

    add no bc option

---
+++ src/shapeopt/Control_to_Trafo/Boundary_Operator/NoBCOperator.py
@@ -0,0 +1,16 @@
+from dolfin import *
+from .BoundaryOperator import BoundaryOperator
+import numpy as np
+
+class NoBCO(BoundaryOperator):
+    def __init__(self, dmesh, dnormal, lb_off):
+        super().__init__(dmesh, dnormal, lb_off)
+
+    def eval(self, x):
+        # x: corresponds to control in self.Vd
+        return x
+
+    def chainrule(self, djy):
+        # compute derivative of j(vector_laplace_beltrami(x)) under the knowledge of
+        # djy = nabla j(y) (gradient)
+        return djy
