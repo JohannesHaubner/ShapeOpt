c8e0b424fd6a8ef65fd57f119c09648625d6c4cc
JohannesHaubner <haubnerj@simula.no>
2023-01-12 07:55:14 +0000

    update github action to publish docker file

--- src/shapeopt/Control_to_Trafo/Boundary_Operator/__init__.py
+++ src/shapeopt/Control_to_Trafo/Boundary_Operator/__init__.py
@@ -1,6 +1,8 @@
 from .LaplaceBeltrami import LaplaceBeltrami
 from .LaplaceBeltrami_withbc import LaplaceBeltrami_withbc
+from .NoBCOperator import NoBCO
 
 boundary_operators = {'laplace_beltrami': LaplaceBeltrami,
                       'laplace_beltrami_withbc': LaplaceBeltrami_withbc,
+                      'no_boundary_operator': NoBCO,
                       }
