a74f8801ec2dafe758603f46e8e171e582c5d82a
JohannesHaubner <haubnerj@simula.no>
2023-07-18 13:44:09 +0200

    update visualization_simulation

--- example/FSI/visualization/visualize_simulation.py
+++ example/FSI/visualization/visualize_simulation.py
@@ -1,20 +1,44 @@
 import pyvista
+import numpy as np
 from colormaps import cmap_1, cmap_2
 
-# specify output directory 
-from pathlib import Path
-here = Path(__file__).parent.parent.resolve()
-
-import sys, os
-sys.path.insert(0, str(here))
-
 # specify filename and read mesh    
-filename = str(here) + "/Output/Forward/init/velocity2.pvd"
+filename = "../Output/Forward/init/velocity2.pvd"
 print(filename)
 
 reader = pyvista.get_reader(filename)
 
-reader.time_values
+# filename for mp4
+mp4_fn = "init_fsi.mp4"
 
-mesh = reader.read()
-mesh.plot()
+# plotter
+pl = pyvista.Plotter()
+# open a movie file
+pl.open_movie(mp4_fn)
+
+# test
+reader.set_active_time_point(2)
+print('active time value:', reader.active_time_value)
+mesh = reader.read()[0]
+mag = np.sum(np.abs(mesh["velocity"])**2, axis=-1)**(1./2)
+mesh.point_data.set_scalars(mag, name="magnitude")
+mesh.point_data.remove("velocity")
+print(mesh.active_scalars)
+print(mesh.array_names)
+print(reader.active_datasets)
+for i in range(2): #range(len(reader.time_values)):
+    reader.set_active_time_point(i)
+    print('active time value:', reader.active_time_value)
+    mesh = reader.read()[0]
+    mag = np.sum(np.abs(mesh["velocity"])**2, axis=-1)**(1./2)
+    mesh.point_data.set_scalars(mag, name="magnitude")
+    mesh.point_data.remove("velocity")
+    print(reader.active_datasets)
+    pl.view_xy()
+    pl.add_mesh(mesh, cmap=cmap_1)
+    pl.enable_image_style()
+    pl.reset_camera_clipping_range()
+    pl.camera.tight()
+    pl.write_frame()
+    
+pl.close()
