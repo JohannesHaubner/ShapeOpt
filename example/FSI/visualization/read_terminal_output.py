16c05da7cf47a24ffe9493efcd5fdcb6a84d1920
root <johannes.haubner@icloud.com>
2023-07-30 13:28:34 +0000

    update read_terminal

--- example/FSI/visualization/read_terminal_output.py
+++ example/FSI/visualization/read_terminal_output.py
@@ -13,7 +13,7 @@ def generate_table(txtname : str, txtout : str, txtout_all : str):
     # list of line starts
     start = ['Objective', 'Dual', 'Constraint', 'Variable', 'Complementarity', 'Overall', 'Number', 'Total', 'EXIT', 'Warning']
 
-    start_ov_wo_reg = ['Objective value without penalization']
+    start_ov_wo_reg = ['Objective value']
 
     start_num = []
     for i in range(10):
@@ -78,14 +78,28 @@ def generate_table(txtname : str, txtout : str, txtout_all : str):
     def remove_whitespace_from_list(ell):
         ell = [list(filter((' ').__ne__, k)) for k in ell]
         return ell
+    
+    def add_previous_line_objective(ell):
+        new_list = []
+        old_line = []
+        for i in ell:
+            if i[2] == str('at'):
+                new_list.append(old_line)
+            else:
+                old_line = i
+        return new_list
 
-    data_list = [important_lines, data, header_ipopt, ob_val]
+    data_list = [important_lines, data, header_ipopt, ob_val, all]
     data_list = [reduce_list(i) for i in data_list] # remove duplicates and \n at end
     for i in [1,2,3]:
         data_list[i] = split_at_whitespace(data_list[i])
     data_list[1] = clean_ipopt_data(data_list[1]) # since lines coming from IPOPT output typically have 20 sub-strings at this point and entry data[i][12] == '-' - remove all other lines that do not fulfill this
     for i in [1,2,3]:
         data_list[i] = remove_whitespace_from_list(data_list[i]) # remove all 
+
+    # postprocess data_list[3] remove all lines that are close to being identity 
+    data_list[3] = add_previous_line_objective(data_list[3])
+
 
     data_array = []
     #data = data_list[2]
@@ -105,8 +119,8 @@ def generate_table(txtname : str, txtout : str, txtout_all : str):
     file_all_txt = os.path.join(str(here) + '/visualization/', txtout_all)
 
     with open(file_all_txt, 'w') as file:
-        for i in range(len(all)):
-            file.write(all[i])
+        for i in range(len(data_list[4])):
+            file.write(data_list[4][i] + '\n')
 
     # write into txt.file which contains latex table code
     with open(file_txt, 'w') as file:
@@ -116,7 +130,10 @@ def generate_table(txtname : str, txtout : str, txtout_all : str):
         file.write("    \\rowcolor{tumblues2}\n")
         file.write("    \\textcolor{white}{iteration} & \\textcolor{white}{objective}& \\textcolor{white}{objective \mbox{ \small w/o reg. \& pen.}}& \\textcolor{white}{dual infeasibility} & \\textcolor{white}{linesearch-steps} &\\\\[0.5ex]\n")
         for i in range(len(data_array)):
-            file.write("%s & $%s$ & $%s$ & $%s$ & $%s$ & \\\\[0.5ex]\n" % (data_array[i][0], data_array[i][1], data_array[i][-1], data_array[i][3], data_array[i][-2]))
+            if i > 1:
+                file.write("%s & $%s$ & $%s$ & $%s$ & $%s$ & \\\\[0.5ex]\n" % (data_array[i][0], data_array[i][1], data_array[i][-1], data_array[i][3], data_array[i][-2]))
+            else:
+                file.write("%s & $%s$ & $%s$ & $%s$ & $%s$ & \\\\[0.5ex]\n" % (data_array[i][0], data_array[i][1], data_array[i][-1], data_array[i][3], data_array[i][-2]))
             if i%2 == 0:
                 file.write("    \\rowcolor{tumg}\n")
         file.write("  \\end{tabularx}\n")
