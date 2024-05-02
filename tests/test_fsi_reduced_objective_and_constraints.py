78d5cad9e71038f0bb967e999e5b73f4908e5eb8
root <johannes.haubner@icloud.com>
2024-05-02 08:24:34 +0000

    update fsi test

--- tests/test_fsi_reduced_objective_and_constraints.py
+++ tests/test_fsi_reduced_objective_and_constraints.py
@@ -3,21 +3,17 @@ from pyadjoint import *
 import numpy as np
 import pytest
 
-if __name__ == '__main__':
-    import sys
-    from pathlib import Path
-    here = Path(__file__).parent
-    sys.path.insert(0, str(here.parent) + '/src')
-
 import shapeopt.Tools.settings_mesh as tsm
 
 from shapeopt.Constraints import constraints
 from shapeopt.Control_to_Trafo import Extension
 from shapeopt.Reduced_Objective import reduced_objectives
+from shapeopt.Control_to_Trafo.Boundary_Operator import boundary_operators
+from shapeopt.Control_to_Trafo.Extension_Operator import extension_operators
 
 from pathlib import Path
 here = Path(__file__).parent
-path_mesh =  str(here.parent) + "/example/FSI/mesh"
+path_mesh =  str(here.parent) + "/example/Stokes/mesh"
 
 #load mesh
 init_mfs = tsm.Initialize_Mesh_and_FunctionSpaces(path_mesh=path_mesh)
@@ -30,8 +26,8 @@ dnormal = init_mfs.get_dnormalf()
 
 # set and load parameters
 geom_prop = np.load(path_mesh + '/geom_prop.npy', allow_pickle='TRUE').item()
-param = {"reg": 1e-1, # regularization parameter
-         "lb_off_p": Constant(1.0), #Laplace Beltrami weighting
+param = {"reg": 1e-2, # regularization parameter
+         "lb_off_p": 1.0, #Laplace Beltrami weighting
          "Vol_D": geom_prop["volume_hold_all_domain"], # volume parameter
          "Bary_D": geom_prop["barycenter_hold_all_domain"], # barycenter
          "Vol_O": geom_prop["volume_obstacle"],
@@ -39,22 +35,22 @@ param = {"reg": 1e-1, # regularization parameter
          "Bary_O": geom_prop["barycenter_obstacle"],
          "L": geom_prop["length_pipe"],
          "H": geom_prop["heigth_pipe"],
+         "Vol_solid": 1., # random value to make volume_solid constraint test work
+         "solid": 5, # for volume_solid constraint test
          "relax_eq": 0.0, #relax barycenter
          #"Bary_eps": 0.0, # slack for barycenter
          "det_lb": 2e-1, # lower bound for determinant of transformation gradient
-         "maxiter_IPOPT": 1, #50,
-         "T": 0.04, #15.0, # simulation horizon for Fluid-Structure interaction simulation
-         "deltat": 0.01, # time step size
-         "gammaP": 1e-3, # penalty parameter for determinant constraint violation
-         "etaP": 0.2, # smoothing parameter for max term in determinant const. violation
-         "output_path": path_mesh + "/Output/", # folder where intermediate results are stored
+         "maxiter_IPOPT": 50
          }
 
 # specify boundary and extension operator (use Extension.print_options())
 boundary_option = 'laplace_beltrami'
 extension_option = 'linear_elasticity'
+boundary_operator = boundary_operators[boundary_option](dmesh, dnormal, Constant(0.5))
+extension_operator = extension_operators[extension_option](mesh, boundaries, params)
+dof_to_trafo = Extension(init_mfs, boundary_operator, extension_operator)
 # governing equations
-application = 'fluid_structure' #'fluid structure' needs to be tested: if no fluid domain assigned --> error since no fluid part of domain
+application = 'stokes' #'fluid structure' needs to be tested: if no fluid domain assigned --> error since no fluid part of domain
 
 # constraints
 ids = []
@@ -66,14 +62,11 @@ for key, value in constraints.items():
 )
 def test_constraints(id):
     print('test constraint \t', id)
-    order, diff = constraints[id](init_mfs, param, boundary_option, extension_option).test()
+    order, diff = constraints[id](init_mfs, param, dof_to_trafo).test()
     assert order > 1.8 or diff < 1e-12
 
-@pytest.mark.skip(reason="this test takes too much time")
-def test_fsi():
-    print('test fsi')
+
+def test_stokes():
+    print('test stokes')
     order, diff = reduced_objectives[application].test(init_mfs, param)
     assert order > 1.8 or diff < 1e-12
-
-if __name__ == "__main__":
-    test_fsi()
