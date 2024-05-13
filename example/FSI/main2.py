2122601d3963ac98082aad177c9121e3a8ee0fe9
root <johannes.haubner@icloud.com>
2024-05-13 08:50:58 +0000

    fix bug for XDMFFile write

--- example/FSI/main2.py
+++ example/FSI/main2.py
@@ -36,7 +36,7 @@ constraint_ids = ['volume_solid'] #needs to be a list
 
 # set and load parameters
 geom_prop = np.load(path_mesh + '/geom_prop.npy', allow_pickle='TRUE').item()
-param = {"reg": 1e-1, # regularization parameter
+param = {"reg": 10, # regularization parameter
          "lb_off_p": Constant(1.0), #Laplace Beltrami weighting
          "Vol_D": geom_prop["volume_hold_all_domain"], # volume parameter
          "Bary_D": geom_prop["barycenter_hold_all_domain"], # barycenter
@@ -47,9 +47,9 @@ param = {"reg": 1e-1, # regularization parameter
          "H": geom_prop["heigth_pipe"],
          "relax_eq": 0.0, #relax barycenter
          #"Bary_eps": 0.0, # slack for barycenter
-         "det_lb": 2e-1, # lower bound for determinant of transformation gradient
-         "maxiter_IPOPT": 50,
-         "T": 15.0, # simulation horizon for Fluid-Structure interaction simulation
+         "det_lb": 5e-1, # lower bound for determinant of transformation gradient
+         "maxiter_IPOPT": 0, #50,
+         "T": 0.01, #15.0, # simulation horizon for Fluid-Structure interaction simulation
          "deltat": 0.01, # time step size
          "gammaP": 1e-3, # penalty parameter for determinant constraint violation
          "etaP": 0.2, # smoothing parameter for max term in determinant const. violation
@@ -150,9 +150,31 @@ if __name__ == "__main__":
 
         print("FSI_main completed", flush=True)
 
-    deformation = dof_to_trafo.dof_to_deformation_precond(init_mfs.vec_to_Vd(x0)) 
-    defo = project(deformation, Vn)
+    boundary_operator = boundary_operators[boundary_option](dmesh, dnormal, Constant(lb_off))
+    extension_operator = extension_operators[extension_option](mesh, boundaries, params, opt_inner_bdry=True)
+    dof_to_trafo = ctt.Extension(init_mfs, boundary_operator, extension_operator)
+    deformation = dof_to_trafo.dof_to_deformation_precond(init_mfs.vec_to_Vd(x0))
+    defo = deformation # project(deformation, Vn)
 
-    with XDMFFile(MPI.comm_world, path_mesh + "/2final_mesh.xdmf") as outfile:
-        outfile.write(mesh)
-        outfile.write_checkpoint(defo, "defo", 0, append=True)
+    # move mesh and save moved mesh
+    ALE.move(mesh, defo, annotate=False)
+    new_mesh = Mesh(mesh)
+
+    mvc2 = MeshValueCollection("size_t", new_mesh, 2)
+    new_domains = cpp.mesh.MeshFunctionSizet(new_mesh, mvc2)
+    new_domains.set_values(domains.array())
+
+    mvc = MeshValueCollection("size_t", new_mesh, 1)
+    new_boundaries = cpp.mesh.MeshFunctionSizet(new_mesh, mvc)
+    new_boundaries.set_values(boundaries.array())
+
+    xdmf = XDMFFile(MPI.comm_self, path_mesh + "/mesh_triangles_final.xdmf")
+    xdmf2 = XDMFFile(MPI.comm_self, path_mesh + "/facet_mesh_final.xdmf")
+    xdmf3 = XDMFFile(MPI.comm_self, path_mesh + "/domains_final.xdmf")
+    xdmf.write(new_mesh)
+    xdmf2.write(new_boundaries)
+    xdmf3.write(new_domains)
+
+
+    #defo = project(deformation, Vn)
+    #bdfile << defo
