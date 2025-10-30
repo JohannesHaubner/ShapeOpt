8f434faa17ef3d359b44f712b063e214d96e6c29
root <johannes.haubner@icloud.com>
2025-10-30 13:13:09 +0000

    update mesh setting, 1st time-step running with approx. 110GB RAM

--- example/FSI/create_mesh_FSI_3d.py
+++ example/FSI/create_mesh_FSI_3d.py
@@ -138,9 +138,9 @@ distance = gmsh.model.mesh.field.add("Distance")
 gmsh.model.mesh.field.setNumbers(distance, "FacesList", fine_resolution_surface_)
 #gmsh.model.mesh.field.setNumbers(distance, "FacesList", obstacle_solid)
 
-resolution = r/4.5
-alpha = 3.4
-beta = 5
+resolution = r/6.5
+alpha = 1.5
+beta = 3.5 #43
 threshold = gmsh.model.mesh.field.add("Threshold")
 gmsh.model.mesh.field.setNumber(threshold, "IField", distance)
 gmsh.model.mesh.field.setNumber(threshold, "LcMin", resolution)
