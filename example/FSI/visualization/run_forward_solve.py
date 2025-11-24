ea6cc753ddb9dc844f6f828a82a07ac2acebc445
JohannesHaubner <johannes.haubner@icloud.com>
2025-11-24 12:08:42 +0000

    update forward solve script

--- example/FSI/visualization/run_forward_solve.py
+++ example/FSI/visualization/run_forward_solve.py
@@ -87,4 +87,5 @@ def run_forward(initial : bool, T : float, deltat : float, path_mesh : str, outp
 
 if __name__ == "__main__":
     T = 15.0
-    run_forward(initial=True, T=T, deltat=0.01, path_mesh="example/FSI/mesh3d", output_folder="3d_forward_3", point=[0.4, 0.2, 0.125], drag=True)
+    run_forward(initial=True, T=T, deltat=0.01, path_mesh="example/FSI/mesh", output_folder="forward", point=[0.6, 0.2], drag=True)
+    #run_forward(initial=True, T=T, deltat=0.01, path_mesh="example/FSI/mesh3d", output_folder="3d_forward_3", point=[0.4, 0.2, 0.125], drag=True)
